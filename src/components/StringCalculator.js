22022354dc3bd1dc8610ec0249cd3671318e2315
Navodit Bharat <navoditbharat@gmail.com>
2024-10-06 20:03:40 +0530

    added button and input text

--- src/components/StringCalculator.js
+++ src/components/StringCalculator.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
 const StringCalculator = () => {
+  const [numbersInput, setNumbersInput] = useState("");
+
   return (
     <div>
       <h2>String Calculator</h2>
+      <div style={{ marginBottom: "10px" }}>
+        <InputText
+          value={numbersInput}
+          onChange={(e) => setNumbersInput(e.target.value)}
+          placeholder="Enter numbers with delimiters"
+          style={{ width: "300px" }}
+        />
+      </div>
+      <Button label="Calculate" icon="pi pi-calculator" onClick={() => {}} />
     </div>
   );
 };
