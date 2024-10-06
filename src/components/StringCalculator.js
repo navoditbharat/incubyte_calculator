17afc554f3faf16061293de841c99dcba4bb3abe
Navodit Bharat <navoditbharat@gmail.com>
2024-10-06 20:08:19 +0530

    added code to pass comma delimiter tc

--- src/components/StringCalculator.js
+++ src/components/StringCalculator.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
+import { Message } from "primereact/message";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 
+import { addNumbersFromString } from "../helpers/calculatorHelper";
+
 const StringCalculator = () => {
   const [numbersInput, setNumbersInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const calculateSum = () => {
+    try {
+      const result = addNumbersFromString(numbersInput);
+      setResult(result);
+    } catch (e) {
+      setResult(null);
+    }
+  };
 
   return (
     <div>
@@ -18,7 +31,17 @@ const StringCalculator = () => {
           style={{ width: "300px" }}
         />
       </div>
-      <Button label="Calculate" icon="pi pi-calculator" onClick={() => {}} />
+      <Button
+        label="Calculate"
+        icon="pi pi-calculator"
+        onClick={calculateSum}
+      />
+
+      {result !== null && (
+        <div style={{ marginTop: "20px" }}>
+          <Message severity="success" text={`Result: ${result}`} />
+        </div>
+      )}
     </div>
   );
 };
