97010e512e0201a3261a21b467446222fbc3ff57
Navodit Bharat <navoditbharat@gmail.com>
2024-10-06 19:31:41 +0530

    added code to pass test cases

--- src/helpers/calculatorHelper.js
+++ src/helpers/calculatorHelper.js
@@ -1,3 +1,6 @@
 export function addNumbersFromString(numbers) {
   if (!numbers) return 0;
+  let delimiter = /,|\n/;
+  const numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
+  return numArray.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
 }
